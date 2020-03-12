fd9c632fca4fcc39632bcecf6e9f8899b1cd1887
Calum Ogilvie <55081509+calumfrogspark@users.noreply.github.com>
2020-03-12 22:17:31 +0000

    adding minify back to JS

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -23,8 +23,7 @@ function js() {
     .pipe(concat('bundle.min.js'))
     .pipe(sourcemaps.init())
     .pipe(sourcemaps.write())
-    // .pipe(uglify())
-    // .pipe(rename('bundle.min.js'))
+    .pipe(uglify())
     .pipe(dest(`${themeURL}js/dist`));
 }
 function sassfn() {
