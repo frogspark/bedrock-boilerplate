b483ae44c006d99b14c621ef3dec29a53d5d0b70
Calum Ogilvie <55081509+calumfrogspark@users.noreply.github.com>
2020-08-10 15:04:31 +0100

    adding base hero file

---
+++ web/app/themes/frogspark/inc/_hero.php
@@ -0,0 +1,9 @@
+<section class="hero">
+  <div style="background-image: url('<?php echo get_field('hero_image')['url']; ?>');">
+    <div class="container">
+      <div class="row">
+  
+      </div>
+    </div>
+  </div>
+</section>
