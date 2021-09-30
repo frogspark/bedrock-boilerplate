e0b2e8dba5f313d835988e0d2c44050a6da974d9
Calum Ogilvie <55081509+calumfrogspark@users.noreply.github.com>
2021-09-30 12:49:30 +0100

    addng debug display to development config file

--- config/environments/development.php
+++ config/environments/development.php
@@ -9,5 +9,6 @@ ini_set('display_errors', 1);
 
 Config::define('SAVEQUERIES', true);
 Config::define('WP_DEBUG', true);
+Config::define('WP_DEBUG_DISPLAY', true);
 Config::define('SCRIPT_DEBUG', true);
 Config::define('DISALLOW_FILE_MODS', false);
